aae00e2ca6815e78e25a943c6a8a02db59c8e4d4
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-03-10 16:40:24 -0600

    add start script and env variable for api call

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get('/test', async (req, res) => {
     try {
-        const response = await axios.get('http://test.com');
+        const response = await axios.get(process.env.LAMBDA_ENDPOINT);
         res.send(response.data);
     } catch (error) {
         res.status(500).send(error.toString());
