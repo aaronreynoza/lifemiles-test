b508d458b40eb15e502d55f7d06a39ce05a8fc61
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-03-10 16:36:00 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.get('/test', async (req, res) => {
+    try {
+        const response = await axios.get('http://test.com');
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
