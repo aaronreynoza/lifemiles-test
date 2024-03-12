f476a0a7e8c368b90a59f171dde16c733e3f863c
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-03-11 23:30:19 -0600

    fix fs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.get('/', async (req, res) => {
     try {
