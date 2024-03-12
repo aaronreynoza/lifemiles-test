125faf890a8668d1cd91377ffb3922b830f8ab90
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2024-03-11 23:24:02 -0600

    add test-data

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ app.get('/', async (req, res) => {
         const lamda = process.env.LAMBDA_ENDPOINT;
         if (lamda) {
             const response = await axios.get(process.env.LAMBDA_ENDPOINT);
-            res.status(200).send(response.data);
+            fs.readFile('test-data/README.md', 'utf8', (err, data) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+                res.status(200).send({
+                    time: response.data,
+                    table: data
+                });
+              });
         } else {
             res.status(200).send({status: "ok", message: "Lambda ENV variable not set!"});
         }
---
+++ test-data/README.md
@@ -0,0 +1,15 @@
+# Customer Data
+
+| CustID | Name |
+|--------|------|
+| 13 | Customer13 |
+| 62 | Customer62 |
+| 444 | Customer444 |
+| 485 | Customer485 |
+| 614 | Customer614 |
+| 747 | Customer747 |
+| 865 | Customer865 |
+| 867 | Customer867 |
+| 910 | Customer910 |
+| 976 | Customer976 |
+| 1021 | Martha Rivera |
